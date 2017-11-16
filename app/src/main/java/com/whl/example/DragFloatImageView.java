a1c265870621d3846a69276305b89eb282cf2121
wanghailong <172323705@qq.com>
2017-11-16 17:10:39 +0800

    Initial commit

---
+++ app/src/main/java/com/whl/example/DragFloatImageView.java
@@ -0,0 +1,107 @@
+package com.whl.example;
+
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageButton;
+
+/**
+
+ * Desc:可拖拽的ImageView
+ */
+
+public class DragFloatImageView extends ImageButton {
+
+    private int parentHeight;
+    private int parentWidth;
+    private int lastX;
+    private int lastY;
+    private boolean isDrag;
+
+    public DragFloatImageView(Context context) {
+        super(context);
+    }
+
+    public DragFloatImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DragFloatImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int rawX = (int) event.getRawX();
+        int rawY = (int) event.getRawY();
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                setPressed(true);
+                isDrag = false;
+                getParent().requestDisallowInterceptTouchEvent(true);
+                lastX = rawX;
+                lastY = rawY;
+                ViewGroup parent;
+                if (getParent() != null) {
+                    parent = (ViewGroup) getParent();
+                    parentHeight = parent.getHeight();
+                    parentWidth = parent.getWidth();
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (parentHeight <= 0 || parentWidth == 0) {
+                    isDrag = false;
+                    break;
+                } else {
+                    isDrag = true;
+                }
+                int dx = rawX - lastX;
+                int dy = rawY - lastY;
+                //这里修复一些手机无法触发点击事件
+                int distance = (int) Math.sqrt(dx * dx + dy * dy);
+                if (distance < 3) {
+                    isDrag = false;
+                    break;
+                }
+                float x = getX() + dx;
+                float y = getY() + dy;
+                //检测是否到达边缘，左上右下
+                x = x < 0 ? 0 : x > parentWidth - getWidth() ? parentWidth - getWidth() : x;
+                y = getY() < 0 ? 0 : getY() + getHeight() > parentHeight ? parentHeight - getHeight() : y;
+                setX(x);
+                setY(y);
+                lastX = rawX;
+                lastY = rawY;
+                break;
+            case MotionEvent.ACTION_UP:
+                if (!isNotDrag()) {
+                    setPressed(false);
+                    if (rawX >= parentWidth / 2) {
+                        //靠右吸附
+                        ObjectAnimator oa = ObjectAnimator.ofFloat(this, "x", lastX, parentWidth - getWidth());
+                        oa.setInterpolator(new DecelerateInterpolator());
+                        oa.setDuration(500);
+                        oa.start();
+                    } else {
+                        //靠左吸附
+                        ObjectAnimator oa = ObjectAnimator.ofFloat(this, "x", getX(), 0);
+                        oa.setInterpolator(new DecelerateInterpolator());
+                        oa.setDuration(500);
+                        oa.start();
+                    }
+                }
+                break;
+        }
+        return !isNotDrag() || super.onTouchEvent(event);
+    }
+
+    private boolean isNotDrag() {
+        return !isDrag && (getX() == 0
+                || (getX() == parentWidth - getWidth()));
+    }
+
+}
